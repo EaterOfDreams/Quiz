362519d2d26e5c60df811c5731203c9a9850f99d
Gloriouss STalen <jaykemanian@gmail.com>
2017-09-27 13:14:47 -0700

    Code or gulag

--- app/src/main/java/com/example/per6/quiz/MainActivity.java
+++ app/src/main/java/com/example/per6/quiz/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.per6.quiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView questionText;
     List<Questioning> questionBank;
     int index, score;
+    public static final String KEY_0 = "2", TAG = "MainActivity", KEY_1 = "34";
 
 
     @Override
@@ -29,8 +33,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         wireWidgets();
         intitQuestionBank();
         setListeners();
+        if (savedInstanceState != null)
+        {
+            index = savedInstanceState.getInt("current question", 0);
+
+            if ((questionBank = savedInstanceState.getParcelableArrayList(KEY_1)) != null)
+                Log.d(TAG, "Work? " + questionBank.size());
+            questionText.setText(questionBank.get(index).getQuestionText());
+
+        }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: method fired");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: method fired");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: method fired");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: method fired");
+        outState.putInt("current question", index);
+        outState.putParcelableArrayList(KEY_1, (ArrayList<? extends Parcelable>) questionBank);
+    }
 
     private void intitQuestionBank() {
         questionBank = new ArrayList();
@@ -85,16 +123,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                index++;
-                if (questionBank.size() <= index-1) {
+
+                if (index+1>=questionBank.size()) {
 //                    trueButton.setVisibility(View.INVISIBLE);
 //                    falseButton.setVisibility(View.INVISIBLE);
 //                    questionText.setText("Your score is:" + score);
-
+                    startActivity(new Intent(MainActivity.this, ScoreActivity.class).putExtra(KEY_0, score));
                 }
-                questionText.setText(questionBank.get(index).getQuestionText());
-                trueButton.setEnabled(true);
-                falseButton.setEnabled(true);
+                else
+                {
+                    index++;
+                    questionText.setText(questionBank.get(index).getQuestionText());
+                    trueButton.setEnabled(true);
+                    falseButton.setEnabled(true);
+                }
+
             }
         });
     }
--- app/src/main/java/com/example/per6/quiz/Questioning.java
+++ app/src/main/java/com/example/per6/quiz/Questioning.java
@@ -1,10 +1,13 @@
 package com.example.per6.quiz;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by per6 on 9/15/17.
  */
 
-public class Questioning {
+public class Questioning implements Parcelable {
     private String questionText;
     private boolean answer;
 
@@ -12,6 +15,24 @@ public class Questioning {
         this.questionText = questionText;
         this.answer = answer;
     }
+
+    protected Questioning(Parcel in) {
+        questionText = in.readString();
+        answer = in.readByte() != 0;
+    }
+
+    public static final Creator<Questioning> CREATOR = new Creator<Questioning>() {
+        @Override
+        public Questioning createFromParcel(Parcel in) {
+            return new Questioning(in);
+        }
+
+        @Override
+        public Questioning[] newArray(int size) {
+            return new Questioning[size];
+        }
+    };
+
 
     public String getQuestionText() {
         return questionText;
@@ -27,5 +48,16 @@ public class Questioning {
      */
     public boolean checkAnswer (boolean userAnswer) {
         return answer == userAnswer;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(questionText);
+        parcel.writeByte((byte) (answer ? 1 : 0));
     }
 }
